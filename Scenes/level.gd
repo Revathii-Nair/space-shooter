ff1a0af4b13a37ebb772d3dc1c5254246aed4426
Revathi Nair <revathiinair2000@gmail.com>
2026-05-02 15:51:26 +0530

    added star animation

--- Scenes/level.gd
+++ Scenes/level.gd
@@ -9,6 +9,24 @@ var laser_scene: PackedScene = load("res://Scenes/laser.tscn")
 func _ready() -> void:
 	get_tree().call_group('ui','set_health',health)
 	$BGMusic.play()
+	
+	# star animation 
+	var size := get_viewport().get_visible_rect().size
+	var rng := RandomNumberGenerator.new()
+	# generating star sprites
+	for i in rng.randi_range(10,30):
+		var newSprite = AnimatedSprite2D.new()
+		newSprite.sprite_frames = $Stars/StarImage.sprite_frames
+		$Stars.add_child(newSprite)
+		newSprite.frame = rng.randi_range(0,23)
+	# randomizing star position
+	for star in $Stars.get_children():
+		var rand_x := rng.randf_range(0,size.x)
+		var rand_y := rng.randf_range(0,size.y)
+
+		star.position = Vector2(rand_x,rand_y)
+		star.play() # also means autoplay
+		
 # to randomize meteor creation
 func _on_meteor_timer_timeout() -> void:
 	#2. Create an instance
@@ -18,6 +36,7 @@ func _on_meteor_timer_timeout() -> void:
 	
 	# since we are creating meteor nodes through code we need to connect custom signals using connect
 	meteor.connect('collision',_on_meteor_collision)
+	 
 	
 # health updation and changing layout to game over layout 
 func _on_meteor_collision():
@@ -27,7 +46,6 @@ func _on_meteor_collision():
 	if health <=0:
 		get_tree().change_scene_to_file("res://Scenes/game_over.tscn")	
 	
-
 
 # custom signal function from player scene
 func _on_player_laser(pos: Variant) -> void:
